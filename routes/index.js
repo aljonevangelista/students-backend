d1e840c51b7dc2afc6b328eaf172bb29914445fd
aljonevangelista <aljonevangelista098@gmail.com>
2026-03-23 20:18:47 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,12 @@
-const express = require ('express');
-router = express.Router();
-const userController = require('../controllers/userContoller');
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController.js');
 
+router.get('/users', userController.getAllUsers);
+router.get('/users/:id', userController.getUserById);
+router.get('/users/course/:course', userController.getUserByCourse);
+router.post('/users', userController.createUser);
+router.put('/users', userController.updateUser);
+router.delete('/users', userController.deleteUser);
 
-//route to get all users
-router.get('/users', userControllers.getAllUsers);
-//Route to serach ID
-router.get('/users/:id', userControllers.getUserById);
-//Route to serach genre
-router.get('/users/:genre', userControllers.getUserByGenre);
-
-
-//Route to create new user
-router.post('/users',userControllers.createUser);
-router.put('/users', userControllers.updateUser);
-
-
-//router delete user
-
-
-router.delete('/users', userControllers.deleteUser);
-module.exports=router;
+module.exports = router;
