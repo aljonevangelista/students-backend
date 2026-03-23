4eb2e674196e03aaf4096a4c6aed3c5dd7b7adc2
aljonevangelista <aljonevangelista098@gmail.com>
2026-03-23 20:18:02 +0800

    Update userControllers.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -6,7 +6,7 @@ const connection = require ('../config/db.js');
 
 //get all users
 exports.getAllUsers = (req,res)=>{
-    connection.query('SELECT * FROM movie_streaming', (err,rows,fields) => {
+    connection.query('SELECT * FROM userdata', (err,rows,fields) => {
         if (err) throw err;
             res.json(rows);
     });
@@ -14,9 +14,19 @@ exports.getAllUsers = (req,res)=>{
 
 
 //Search a user by Id
-exports.getUserById=(req,res)=> {
-    const id=req.params.id;
-    connection.query('SELECT * FROM movie_streaming WHERE id=?', [id], (err, rows,fields)=> {
+exports.getUserById = (req, res) => {
+    const id = req.params.id;
+    connection.query('SELECT * FROM userdata WHERE id=?', [id], (err, rows) => {
+        if (err) return res.status(500).json({ error: err.message });
+        res.json(rows);
+    });
+};
+
+
+//Search a user by course
+exports.getUserByCourse=(req,res)=> {
+    const course=req.params.course;
+    connection.query('SELECT * FROM userdata WHERE course=?', [course], (err, rows,fields)=> {
         if(err) throw err;
         if(rows.length>0)
             res.json(rows);
@@ -26,30 +36,11 @@ exports.getUserById=(req,res)=> {
     });
 }
 
-
-
-// Search by genre
-exports.getUserByGenre = (req, res) => {
-    const genre = req.params.genre;
-    connection.query('SELECT * FROM movie_streaming WHERE genre = ?', [genre], (err, rows) => {
-        if (err) {
-            console.error('Query error:', err);
-            return res.status(500).json({ error: 'Database error' });
-        }
-        if (rows.length > 0) {
-            res.json(rows);
-        } else {
-            res.status(404).json({ message: 'No records found for this genre' });
-        }
-    });
-};
-
-
 //Create a new User
 //crud - create
 exports.createUser=(req,res)=> {
-    const {user_name, movie_title, genre, subscription_type, device_used, rating} = req.body;
-    connection.query('INSERT INTO movie_streaming (user_name, movie_title, genre, subscription_type, device_used, rating) VALUES ((?, ?, ?, ?, ?, ?)',[user_name, movie_title, genre, subscription_type, device_used, rating], (err,result)=> {
+    const {name, age, course,gender, year} = req.body;
+    connection.query('INSERT INTO userdata (name, age, course, gender, year) VALUES (?,?,?,?)',[name, age, course, gender, year], (err,result)=> {
         if(err) throw err;
         res.json({message:'User Created Successfully', userId:
         result.insertId});
@@ -60,8 +51,8 @@ exports.createUser=(req,res)=> {
 //update
 //crud -update
 exports.updateUser=(req,res)=>{
-    const {user_name, movie_title, genre, subscription_type, device_used, rating} = req.body;
-    connection.query('UPDATE  movie_streaming SET user_name=?, movie_title=?, genre=?, subscription_type=?, device_used=?, rating=? WHERE id=?', [user_name, movie_title, genre, subscription_type, device_used, rating, id], (err,result) => {
+    const {id, name, age, course, gender, year} = req.body;
+    connection.query('UPDATE  userdata SET name=?, age=?, course=?, gender=?, year=?, WHERE id=?', [name, age, course,gender, year,id], (err,result) => {
         if (err) throw err;
         if(result.affectedRows>0)
             res.json({message:'User Update Succesfully'});
@@ -70,15 +61,13 @@ exports.updateUser=(req,res)=>{
     });
 };
 
-
 //delete
 //crud-delete
 
-
 exports.deleteUser=(req,res)=>{
     const {id}  = req.body;
-    
-    connection.query('DELETE FROM movie_streaming WHERE id=?', [id], (err,result) => {
+
+    connection.query('DELETE FROM userdata WHERE id=?', [id], (err,result) => {
         if (err) throw err;
         if(result.affectedRows>0)
             res.json({message:'User Deleted Succesfully'});
