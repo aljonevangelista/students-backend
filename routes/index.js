36c416f0f833e707a1a213bbf661ced14c4d3635
aljonevangelista <aljonevangelista098@gmail.com>
2026-03-23 21:12:29 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userControllers.js');
+const userController = require('../controllers/userController.js');
+
 
 router.get('/users', userController.getAllUsers);
 
+
 router.get('/users/:id', userController.getUserById);
+
 
 router.get('/users/course/:course', userController.getUserByCourse);
 
+
 router.post('/users', userController.createUser);
+
 
 router.put('/users', userController.updateUser);
 
+
 router.delete('/users', userController.deleteUser);
+
 
 module.exports = router;
